86632ca4cb884da62212240e62095b54a7e79d3a
Ifu Aniemeka <ianiemeka@gmail.com>
2014-07-26 12:29:32 -0500

    Basic functionality implemented

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var triviaApp = angular.module('triviaApp', ['firebase', 'ngRoute']);
+
+triviaApp.config(['$routeProvider', function($routeProvider){
+  $routeProvider.when('/questions', {
+    templateUrl: 'partials/question-list.html',
+    controller: "QuestionListCtrl"
+  }).otherwise({
+    redirectTo: '/questions'
+  })
+}]);
+
+triviaApp.controller('QuestionListCtrl', ['$scope', '$firebase',
+  function($scope, $firebase) {
+    var firebaseUrl = "https://crackling-fire-2657.firebaseio.com/questions";
+    $scope.questions = $firebase(new Firebase(firebaseUrl));
+}]);
