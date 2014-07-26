7eed0ac30fdf214f95d90062449f6094c5f1287d
Ifu Aniemeka <ianiemeka@gmail.com>
2014-07-26 13:22:58 -0500

    Load add new questions form partial

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ triviaApp.config(['$routeProvider', function($routeProvider){
   $routeProvider.when('/questions', {
     templateUrl: 'partials/question-list.html',
     controller: "QuestionListCtrl"
+  }).when('/questions/new', {
+    templateUrl: 'partials/question-form.html',
+    controller: 'QuestionNewCtrl'
   }).otherwise({
     redirectTo: '/questions'
   })
@@ -13,4 +16,17 @@ triviaApp.controller('QuestionListCtrl', ['$scope', '$firebase',
   function($scope, $firebase) {
     var firebaseUrl = "https://crackling-fire-2657.firebaseio.com/questions";
     $scope.questions = $firebase(new Firebase(firebaseUrl));
+}]);
+
+triviaApp.controller("QuestionNewCtrl", ['$scope', '$firebase', '$location',
+  function($scope, $firebase, $location){
+    $scope.question = {};
+
+    $scope.persistQuestion = function(question){
+      var firebaseUrl = "https://crackling-fire-2657.firebaseio.com/questions";
+      $scope.questions = $firebase(new Firebase(firebaseUrl));
+      $scope.questions.$add(question).then(function(ref){
+        $location.url('/questions');
+      });
+    };
 }]);
